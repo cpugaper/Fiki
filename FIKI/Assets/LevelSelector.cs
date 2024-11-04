1a96ac0e6287dcdce4cc215efa191dc080395fbe
Aleix3 <Aleixbotella.1@gmail.com>
2024-11-04 09:59:20 +0100

    Pantalla de puntuaciones

--- FIKI/Assets/LevelSelector.cs
+++ FIKI/Assets/LevelSelector.cs
@@ -5,38 +5,64 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Puntuacion_Canvas; 
+
     void Start()
     {
-        
+
     }
 
     public void Minigame1()
     {
-        SceneManager.LoadScene("Minigame");
+        LevelTransitionController.instance.StartTransition(3, 2);
     }
+
     public void Minigame2()
     {
-
+        // Implementar lógica para Minigame2
     }
+
     public void Minigame3()
     {
-
+        // Implementar lógica para Minigame3
     }
+
     public void Minigame4()
     {
-
+        // Implementar lógica para Minigame4
     }
+
     public void Minigame5()
     {
-
+        // Implementar lógica para Minigame5
     }
+
     public void Minigame6()
     {
-
+        // Implementar lógica para Minigame6
     }
+
+    public void Puntuacion()
+    {
+        
+        if (Puntuacion_Canvas != null)
+        {
+            Puntuacion_Canvas.SetActive(true);
+        }
+    }
+
     public void Atras()
     {
-        SceneManager.LoadScene("Lobby");
+        LevelTransitionController.instance.StartTransition(1, 2);
+    }
+
+    public void AtrasPuntuacion()
+    {
+        
+        if (Puntuacion_Canvas != null)
+        {
+            Puntuacion_Canvas.SetActive(false);
+        }
     }
 }
+
