a926c57497f8ae89eaca474d07ad387d8ddd4fcf
Marti Sabaté <sabatemiranda@gmail.com>
2024-11-05 08:46:24 +0100

    MSM - SplashScreen timer for transition
    Ahora hay un timer para que se pueda ver un poco el logo en vez de nomas empezar la escena hacer la transicion

--- FIKI/Assets/Scripts/SplashScreenController.cs
+++ FIKI/Assets/Scripts/SplashScreenController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class SplashScreenController : MonoBehaviour
 {
+    float contador = 0;
+    bool transicion = false;
     // Start is called before the first frame update
     void Start()
     {
-        LevelTransitionController.instance.StartTransition(1, 2);
+        //LevelTransitionController.instance.StartTransition(1, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        contador += Time.deltaTime;
+
+        if(contador > 3 && !transicion)
+        {
+            transicion = true;
+            LevelTransitionController.instance.StartTransition(1, 2);
+        }
     }
 
 
